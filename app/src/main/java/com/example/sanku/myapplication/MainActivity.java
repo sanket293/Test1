014b6ef976b52fd9642c988bd893cc03f8fbceba
sanket293 <scvagadiya@gmail.com>
2018-08-07 13:06:43 -0400

    find view id

--- app/src/main/java/com/example/sanku/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sanku/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sanku.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.tvHelloWorld);
+
     }
 }
